9f51d52511bfddee0a6f0fe226f7e9c90a0373a6
Raj <rajbaidyanath76@gmail.com>
2025-03-12 23:50:26 +0530

    l

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,8 @@
 "use client"; // Required if using Next.js App Router (app directory)
+import Link from "next/link";
 
 import React from 'react';
 import { useSession, signIn, signOut } from "next-auth/react";
-
 const Navbar = () => {
   const { data: session } = useSession();
 
@@ -25,15 +25,11 @@ const Navbar = () => {
             </button>
           </>
         ) : (
-          <button
-            className="text-white bg-gradient-to-br from-purple-600 to-blue-500 
-              hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 
-              dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 
-              text-center me-2 mb-2"
-            onClick={() => signIn("github")}
-          >
-            Login with GitHub
-          </button>
+          <Link href="/login">
+            <button className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2">
+              Login
+            </button>
+          </Link>
         )}
       </div>
     </nav>
