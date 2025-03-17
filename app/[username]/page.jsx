9c1f1a588f7be8e443a8722c23a550305d79d8b8
Raj <rajbaidyanath76@gmail.com>
2025-03-17 21:16:43 +0530

    nmb

--- app/[username]/page.jsx
+++ app/[username]/page.jsx
@@ -7,29 +7,29 @@ const Username = ({ params }) => {
         {/* Main Image */}
         <img
           className="object-cover w-full h-[350px]"
-          src="https://miro.medium.com/v2/resize:fit:1000/1*Nqq8jcQkiuSZkNZHzopbDg.gif"
+          src="https://static.picmaker.com/scene-prebuilts/thumbnails/26422876-6728-480d-bf61-65d7bde0da73.png"
           alt=""
         />
         
         {/* Profile Image */}
-        <div className="absolute -bottom-20 right-[46%] border-white border-2 rounded-full">
+        <div className="absolute -bottom-24 mt-50 right-[46%] border-white border-2 rounded-full">
           <img
-            className="rounded-full"
+            className="rounded-full object-cover"
             width={150}
             height={150}
-            src="https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/National%20Geographic_2572187_square.jpg"
+            src="https://img.freepik.com/premium-photo/3d-icon-rounded-user-profile-blue-orange-perfect-apps-websites-social-media-graphics_1057-196181.jpg"
             alt=""
           />
         </div>
-      </div>
+      </div> 
+
+
 
       {/* Username Info */}
-      <div className="info flex justify-center items-center my-24">
-        <div className="font-bold text-lg">@{params.username}</div>
-      </div>
+     
 
       {/* Info Section */}
-      <div className="info flex justify-center items-center my-24 mb-32 flex-col gap-2">
+      <div className="info flex justify-center items-center my-8 mt-30 flex-col gap-5">
         <div className="font-bold text-lg">@{params.username}</div>
         <div className="text-slate-400">
           Creating Animated Art for VTT's
@@ -40,10 +40,9 @@ const Username = ({ params }) => {
       </div>
 
       {/* Payment Section */}
-      <div className="payment flex gap-3 w-[80%] mt-11">
+      <div className="payment flex gap-3 w-[60%] ml-40 mt-10 ">
         {/* Supporters List */}
-        <div className="supporters w-1/2 bg-slate-900 rounded-lg text-white p-10">
-          {/* Show list of all the supporters as a leaderboard */}
+        <div className="supporters w-1/2 bg-slate-900 rounded-lg text-white p-6">
           <h2 className="text-2xl font-bold my-5">Supporters</h2>
           <ul className="mx-5 text-lg">
             <li className="my-2">
@@ -54,14 +53,11 @@ const Username = ({ params }) => {
             <li className="my-2">Shubham donated $30 with a message ""</li>
             <li className="my-2">Shubham donated $30 with a message ""</li>
             <li className="my-2">Shubham donated $30 with a message ""</li>
-            <li className="my-2">Shubham donated $30 with a message ""</li>
-            <li className="my-2">Shubham donated $30 with a message ""</li>
-            <li className="my-2">Shubham donated $30 with a message ""</li>
           </ul>
         </div>
 
         {/* Make Payment Section */}
-        <div className="makePayment w-1/2 bg-slate-900 rounded-lg text-white p-10">
+        <div className="makePayment w-1/2 bg-slate-900 rounded-lg text-white p-6 h-[50vh]">
           <h2 className="text-2xl font-bold my-5">Make a Payment</h2>
           <div className="flex gap-2 flex-col">
             {/* Input for name and message */}
@@ -91,7 +87,7 @@ const Username = ({ params }) => {
               </button>
             </div>
             {/* Or choose from these amounts */}
-            <div className="flex gap-2 mt-5">
+            <div className="flex gap-2 ">
               <button className="bg-slate-800 p-3 rounded-lg">Pay $10</button>
               <button className="bg-slate-800 p-3 rounded-lg">Pay $20</button>
               <button className="bg-slate-800 p-3 rounded-lg">Pay $30</button>
